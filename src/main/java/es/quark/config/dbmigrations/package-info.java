8af00dd3ca9d1eb6226a29c25cd5b761abb465fa
eduardogm81qg <eduardogmqg@gmail.com>
2018-05-17 12:50:28 +0200

    Initial application generated by JHipster-4.14.4

---
+++ src/main/java/es/quark/config/dbmigrations/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * MongoDB database migrations using MongoBee.
+ */
+package es.quark.config.dbmigrations;
---
+++ src/main/java/es/quark/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package es.quark.repository;
+
+import es.quark.domain.Authority;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Spring Data MongoDB repository for the Authority entity.
+ */
+public interface AuthorityRepository extends MongoRepository<Authority, String> {
+}
